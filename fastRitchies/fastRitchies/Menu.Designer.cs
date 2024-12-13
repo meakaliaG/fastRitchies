f3cb202b3b545a8252a17288b8bdc093085f1976
Olivia Gauthier <org1181@rit.edu>
2024-12-13 09:52:53 -0500

    all category buttons have images

--- fastRitchies/fastRitchies/Menu.Designer.cs
+++ fastRitchies/fastRitchies/Menu.Designer.cs
@@ -69,7 +69,7 @@
             this.MainsButton.Location = new System.Drawing.Point(4, 4);
             this.MainsButton.Margin = new System.Windows.Forms.Padding(4);
             this.MainsButton.Name = "MainsButton";
-            this.MainsButton.Padding = new System.Windows.Forms.Padding(30, 20, 30, 20);
+            this.MainsButton.Padding = new System.Windows.Forms.Padding(20, 0, 20, 0);
             this.MainsButton.Size = new System.Drawing.Size(205, 88);
             this.MainsButton.TabIndex = 0;
             this.MainsButton.Text = "Mains";
@@ -79,34 +79,45 @@
             // 
             // SidesButton
             // 
+            this.SidesButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SidesButton.Image = ((System.Drawing.Image)(resources.GetObject("SidesButton.Image")));
             this.SidesButton.Location = new System.Drawing.Point(217, 4);
             this.SidesButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.SidesButton.Name = "SidesButton";
+            this.SidesButton.Padding = new System.Windows.Forms.Padding(30, 0, 30, 0);
             this.SidesButton.Size = new System.Drawing.Size(205, 88);
             this.SidesButton.TabIndex = 1;
             this.SidesButton.Text = "Sides";
+            this.SidesButton.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
             this.SidesButton.UseVisualStyleBackColor = true;
             this.SidesButton.Click += new System.EventHandler(this.ShowSides);
             // 
             // DrinkButton
             // 
+            this.DrinkButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DrinkButton.Image = ((System.Drawing.Image)(resources.GetObject("DrinkButton.Image")));
             this.DrinkButton.Location = new System.Drawing.Point(430, 4);
             this.DrinkButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.DrinkButton.Name = "DrinkButton";
+            this.DrinkButton.Padding = new System.Windows.Forms.Padding(30, 0, 30, 0);
             this.DrinkButton.Size = new System.Drawing.Size(214, 88);
             this.DrinkButton.TabIndex = 2;
             this.DrinkButton.Text = "Drinks";
+            this.DrinkButton.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
             this.DrinkButton.UseVisualStyleBackColor = true;
             this.DrinkButton.Click += new System.EventHandler(this.ShowDrinks);
             // 
             // DessertButton
             // 
+            this.DessertButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DessertButton.Image = ((System.Drawing.Image)(resources.GetObject("DessertButton.Image")));
             this.DessertButton.Location = new System.Drawing.Point(652, 4);
             this.DessertButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.DessertButton.Name = "DessertButton";
             this.DessertButton.Size = new System.Drawing.Size(178, 88);
             this.DessertButton.TabIndex = 3;
             this.DessertButton.Text = "Desserts";
+            this.DessertButton.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
             this.DessertButton.UseVisualStyleBackColor = true;
             this.DessertButton.Click += new System.EventHandler(this.ShowDesserts);
             // 
